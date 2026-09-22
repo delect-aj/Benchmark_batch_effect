ab260ea853d3012eaa36eab38ef4a711b7e3f7b0
delect-aj <1076169240@qq.com>
2026-09-22 14:38:22 +0800

    conqur_diag: new variant names

--- benchmark/pilot/conqur_diag.R
+++ benchmark/pilot/conqur_diag.R
@@ -2,7 +2,7 @@
 # Usage: Rscript pilot/conqur_diag.R <results dir>
 r <- commandArgs(TRUE)[1]
 f <- list.files(r, "\\.score$", recursive = TRUE, full.names = TRUE)
-f <- f[grepl("null_16s_conf0/|crc_mgx/", f) & grepl("/(raw|oracle|conqur|conqur_permcov|conqur_tuned)\\.score$", f)]
+f <- f[grepl("null_16s_conf0/|crc_mgx/", f) & grepl("/(raw|oracle|conqur|conqur_default|conqur_permcov)\\.score$", f)]
 s <- do.call(rbind, lapply(f, function(x) { d <- read.delim(x); if (ncol(d) < 3) return(NULL)
   d$ds <- basename(dirname(x)); d$scen <- basename(dirname(dirname(x))); d[, c("method", "ds", "scen", "R2_batch_ait", "R2_pheno_ait", "iLISI", "DA_nsig")] }))
 nl <- s[s$scen == "null_16s_conf0", ]
